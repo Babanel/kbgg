46b83190f50ae227bcb69ef0e309c60750a17748
Babanel <drizzt.mrg@gmail.com>
2022-04-06 18:00:13 +0300

    Moved Languages to Utils Language class

--- src/main/kotlin/it/heptartle/kbgg/domain/geekdo/Files.kt
+++ src/main/kotlin/it/heptartle/kbgg/domain/geekdo/Files.kt
@@ -1,9 +1,11 @@
 package it.heptartle.kbgg.domain.geekdo
 
+import it.heptartle.kbgg.Utils.Language
+
 data class Files(val files: FilesContent,
                  val config: FilesConfig)
 
-data class FilesContent(val hot: List<File>)
+data class FilesContent(val list: List<File>)
 
 data class File(val filepageid: String,
                 val fileid: String,
@@ -26,5 +28,3 @@ data class FilesConfig(val endpage: Int,
                        val numitems: Int,
                        val languages: List<Language>)
 
-data class Language(val languageid: String,
-                    val name: String)
