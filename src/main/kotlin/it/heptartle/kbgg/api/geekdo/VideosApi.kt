8944d4c9c7dbe76c955875a668de05622113f2ad
Babanel <drizzt.mrg@gmail.com>
2022-04-07 16:27:10 +0300

    Bug fixes

--- src/main/kotlin/it/heptartle/kbgg/api/geekdo/VideosApi.kt
+++ src/main/kotlin/it/heptartle/kbgg/api/geekdo/VideosApi.kt
@@ -1,6 +1,7 @@
 package it.heptartle.kbgg.api.geekdo
 
 import it.heptartle.kbgg.domain.geekdo.Videos
+import org.intellij.lang.annotations.Language
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -9,14 +10,14 @@ interface VideosApi {
 
     @GET("videos")
     fun getVideos(@Query("objectid") objectid:Int,
-                @Query("ajax") ajax:Int = 1,
-                @Query("nosession") nosession:Int = 1,
-                @Query("objecttype") objecttype:String = "thing",
-                @Query("pageid") pageid:Int = 1,
-                @Query("sorts[0][showcount]") sortshowncount:Int = 50,
-                @Query("sorts[0][sort]") sortshownsort:String = "hot",
-                @Query("languageid") languageid:String = "0",
-                @Query("gallery") gallery:String = "all"
+                  @Query("ajax") ajax:Int = 1,
+                  @Query("nosession") nosession:Int = 1,
+                  @Query("objecttype") objecttype:String = "thing",
+                  @Query("pageid") pageid:Int = 1,
+                  @Query("showcount") sortshowncount:Int = 50,
+                  @Query("sort") sortshownsort:String = "hot",
+                  @Query("languageid") languageid:String = "0",
+                  @Query("gallery") gallery:String = "all"
                 ): Call<Videos>
 }
 
